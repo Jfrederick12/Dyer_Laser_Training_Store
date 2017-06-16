95149802918b16b0c34042d84c4601b78ca013a9
Justin Frederick <jfrederickdesign@gmail.com>
2017-06-16 11:40:49 -0500

    update styling for background images

--- client/app/bundles/HelloWorld/components/Navigation.jsx
+++ client/app/bundles/HelloWorld/components/Navigation.jsx
@@ -5,7 +5,7 @@ class Navigation extends Component {
 	render() {
 		return(
       <nav className="navigation">
-        <Scrollchor to="#top" animate={{offset: -50, duration: 600}} className="nav-link">Top</Scrollchor>
+        <Scrollchor to="#top" animate={{offset: -50, duration: 600}} className="nav-link"><i className="fa fa-heartbeat"></i></Scrollchor>
         <Scrollchor to="#videos" animate={{offset: -50, duration: 600}} className="nav-link">Videos</Scrollchor>
         <Scrollchor to="#benefits" animate={{offset: -50, duration: 600}} className="nav-link">Benefits</Scrollchor>
         <Scrollchor to="#about" animate={{offset: -50, duration: 600}} className="nav-link">About</Scrollchor>
