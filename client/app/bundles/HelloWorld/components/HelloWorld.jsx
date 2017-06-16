81fa42150648b1466dd4db686d33be2111f002f2
Justin Frederick <jfrederickdesign@gmail.com>
2017-06-16 12:27:30 -0500

    update product boxes

--- client/app/bundles/HelloWorld/components/HelloWorld.jsx
+++ client/app/bundles/HelloWorld/components/HelloWorld.jsx
@@ -25,9 +25,9 @@ export default class HelloWorld extends React.Component {
       {
         name: {
           dvd: 'DVD 1',
-          tagline: 'Technique For Laser Crown Lengthening And Lip Lowering Surgery'
+          tagline: 'Periodontal Laser Therapy'
         },
-        description: ['Improve pocket depths of patients without numbing shots', 'Provide comfort to patients who are shy', 'Eliminate the necessity of shots'],
+        description: ['Biologic basis for using diode lasers in periodontal therapy', 'Leveraging lasers for dental implants', 'Also accommodates orthodontic, restorative, and endodontic treatments, as well as oral surgery procedures and wound healing'],
         tagline: 'Would you be interested to learn that you could predictably improve the pocket depths of your patients and never have to administer numbing shots? In dentistry, we’re always searching for new ways to provide the utmost comfort and reassurance to our patients who fear visiting the dentist. Knowing that shots won’t be necessary is one way to provide peace of mind to your patients.',
         chargeAmount: 20000,
         amount: 200,
@@ -36,22 +36,22 @@ export default class HelloWorld extends React.Component {
       {
         name: {
           dvd: 'DVD 2',
-          tagline: 'Technique For Laser Crown Lengthening And Lip Lowering Surgery'
+          tagline: 'Low Level Laser Therapy'
         },
         chargeAmount: 20000,
         amount: 200,
-        description: ['Improve pocket depths of patients without numbing shots', 'Provide comfort to patients who are shy', 'Eliminate the necessity of shots'],
+        description: ['Reduce pain and swelling', 'Analgesia and wound healing', 'Maintain dental implant health, and treat infections'],
         tagline: 'Low-level laser therapy may be the most important laser adjunct to your dental practice. Using this technology makes it possible to reduce pain and swelling in a surgery site and in yours patient’s TMJ.',
         video: 'https://www.youtube.com/embed/K-hfJkLqMLg'
       },
       {
         name: {
           dvd: 'DVD 3',
-          tagline: 'Technique For Laser Crown Lengthening And Lip Lowering Surgery'
+          tagline: 'Laser Crown Lengthening and Lip Lowering Surgery'
         },
         chargeAmount: 20000,
         amount: 200,
-        description: ['Improve pocket depths of patients without numbing shots', 'Provide comfort to patients who are shy', 'Eliminate the necessity of shots'],
+        description: ['Cosmetic contouring of bone and soft tissue', 'Improve symmetry around new crowns and veneers', 'Lip lowering surgery'],
         tagline: 'Dr. Dyer literally wrote the paper on how to use the erbium laser for cosmetic contouring of the bone and soft tissue around teeth.Do you need to improve the appearance of a patient with a high lip line? How about symmetry around your new crowns and veneers?',
         video: 'https://www.youtube.com/embed/K-hfJkLqMLg'
       },    
@@ -62,7 +62,7 @@ export default class HelloWorld extends React.Component {
         },
         chargeAmount: 20000,
         amount: 200,
-        description: ['Improve pocket depths of patients without numbing shots', 'Provide comfort to patients who are shy', 'Eliminate the necessity of shots'],
+        description: ['Laser new attachment procedure', 'Laser guided tissue regeneration'],
         tagline: 'Dr. Dyer literally wrote the paper on how to use the erbium laser for cosmetic contouring of the bone and soft tissue around teeth.Do you need to improve the appearance of a patient with a high lip line? How about symmetry around your new crowns and veneers?',
         video: 'https://www.youtube.com/embed/K-hfJkLqMLg'
       },         
